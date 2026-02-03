9b407e3bb085a53cb5195be616c5876dc18931c3
y754 <freyanye@gmail.com>
2026-02-03 16:23:33 +0000

    Quartz sync: Feb 3, 2026, 4:23 PM

--- content/02 - Lore/index.md
+++
@@ -1,8 +0,0 @@
----
-title: Lore
-description: Notes for lore, items, and characters.
-tags:
- - index
- - lore
-publish: true
----
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -42,7 +42,7 @@ export const defaultContentPageLayout: PageLayout = {
             title: "Contents",
             folderClickBehavior: "collapse",
             folderDefaultState: "open",
-            useSavedState: true
+            useSavedState: true,
             sortFn: (a, b) => {
                 return a.displayName.localeCompare (b.displayName)
             }
