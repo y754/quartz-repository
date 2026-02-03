d0617694c06649e85c8e0c2e6f2d7f0e22db3330
y754 <freyanye@gmail.com>
2026-02-03 12:05:55 +0000

    Quartz sync: Feb 3, 2026, 12:05 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -39,9 +39,9 @@ export const defaultContentPageLayout: PageLayout = {
       ],
     }),
           Component.Explorer({
-            title: "Explorer", // title of the explorer component
+            title: "Contents", // title of the explorer component
             folderClickBehavior: "collapse", // what happens when you click a folder ("link" to navigate to folder page on click or "collapse" to collapse folder on click)
-            folderDefaultState: "collapsed", // default state of folders ("collapsed" or "open")
+            folderDefaultState: "open", // default state of folders ("collapsed" or "open")
             useSavedState: true, // whether to use local storage to save "state" (which folders are opened) of explorer
   // omitted but shown later
             sortFn: ...,
